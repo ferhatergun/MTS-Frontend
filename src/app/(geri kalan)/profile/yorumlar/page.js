f8932182d0b3edd7f0a1d9dfae2fa9ba9d01b5f7
ferhatergun <eferhat718@gmail.com>
2023-08-15 15:16:37 +0300

    profil yorum mimarisi kuruldu dolduruldu

--- src/app/(geri kalan)/profile/yorumlar/page.js
+++ src/app/(geri kalan)/profile/yorumlar/page.js
@@ -6,8 +6,8 @@ export default function page() {
   return (
     <div>
       <div className={styles.container}>
-        <ProfileYorum />
-        <ProfileYorum />
+        <ProfileYorum movieName="Hızlı ve öfkeli 9" star={3} content="çok sevdim" key={1} />
+        <ProfileYorum movieName="Barbie" star={4} content="idare eder eğlenceliydi" key={2}/>
       </div>
     </div>
   )
--- src/components/ProfileYorum/ProfileYorum.js
+++ src/components/ProfileYorum/ProfileYorum.js
@@ -2,15 +2,21 @@ import React from 'react'
 import styles from './page.module.css'
 import Image from 'next/image'
 import img from '$/assets/hizli-ve-ofkeli-9.jpeg'
+import { Rating } from '@mui/material'
+import Link from 'next/link'
 
-export default function ProfileYorum() {
+export default function ProfileYorum({movieName,star,content}) {
   return (
     <div className={styles.container}>
         <div className={styles.Image}>
             <Image src={img} fill={true} />
         </div>
-        <div className={styles.content}></div>
-
+        <div className={styles.contentDiv}>
+          <p className={styles.movieName}>{movieName}</p>
+          <Rating value={star} readOnly sx={{color:'purple'}} />
+          <p className={styles.content}>{content}</p>
+          <Link href="/" className={styles.link}>Yoruma Git</Link>
+        </div>
     </div>
   )
 }
