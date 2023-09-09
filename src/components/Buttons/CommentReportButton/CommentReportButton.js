fb1f1d22575cf875131b8faecbbab547bd6d2f22
ferhatergun <eferhat718@gmail.com>
2023-09-09 03:27:59 +0300

    rapor formu oluşturuldu

--- src/components/Buttons/CommentReportButton/CommentReportButton.js
+++ src/components/Buttons/CommentReportButton/CommentReportButton.js
@@ -4,13 +4,25 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import styles from './page.module.css'
 import ReportOutlinedIcon from '@mui/icons-material/ReportOutlined';
 import { Dropdown, Space } from 'antd';
+import Box from '@mui/material/Box';
+import { TextField,Typography,Modal,InputLabel,MenuItem,FormControl,Select } from '@mui/material';
 
 
 export default function CommentReportButton() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
 
 const items = [
   {
-    label: <div className={styles.reportBtn}><ReportOutlinedIcon/>Rapor Et</div>,
+    label: <div className={styles.reportBtn} onClick={handleOpen}><ReportOutlinedIcon/>Rapor Et</div>,
     key: '0',
   },
 ];
@@ -24,10 +36,53 @@ const items = [
     trigger={['click']}
     className={styles.reportBtn}
     >
-      <Space style={styles.reportBtn}>
+      <Space>
       <MoreVertIcon className={styles.Icon} />
       </Space>
     </Dropdown>
+    {/* açılır report sayfası */}
+    <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Raporla
+          </Typography>
+          <FormControl variant="standard" sx={{ minWidth: 160 ,mt:3}}>
+        <InputLabel id="demo-simple-select-standard-label">Rapor Nedeni</InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={age}
+          onChange={handleChange}
+          label="Age"
+        >
+          <MenuItem value={10}>Uygunsuz İçerik</MenuItem>
+          <MenuItem value={20}>Spam Ve Reklam</MenuItem>
+          <MenuItem value={30}>Yalan Bilgi</MenuItem>
+        </Select>
+      </FormControl>
+        <TextField id="standard-basic" label="Açıklayın" variant="standard" sx={{mt:2}} multiline />
+        <button className={styles.sendBtn}>Gönder</button>
+        </Box>
+      </Modal>
     </>
   )
-}
+}5
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+  borderRadius:5,
+  display:'flex',
+  flexDirection:'column',
+};
