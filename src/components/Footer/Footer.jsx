4eb9b4b6beef2380e3249ab037e598abcee62361
ferhatergun <eferhat718@gmail.com>
2023-12-01 00:25:11 +0300

    footer yeniden inşaa edildi

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,35 +1,104 @@
-import React from 'react'
-import styles from './page.module.css'
-import {Grid} from '@mui/material'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+// import { footerLinks } from "@/constants";
+import styles from "./page.module.css"
 
-export default function Footer() {
-  
+const Footer = () => {
+  const footerLinks = []
   return (
-    <div className={styles.container}>
-      <div className={styles.top}>
-        <Grid container  className={styles.topContainer}>
-          <Grid item xs={5} sm={4} md={3}>
-            <div className={styles.logo}>logo</div>
-            <div className={styles.description}>açıklama</div>
-          </Grid>
-          <Grid item xs={5} sm={4} md={3}>
-            <h4>Hızlı Linkler</h4>
-            <ul className={styles.fastlink}>
-              <li className={styles.link}>En İyi Filmler</li>
-              <li className={styles.link}>En İyi Diziler</li>
-              <li className={styles.link}>Popüler Filmler</li>
-              <li className={styles.link}>Vizyondakiler</li>
-              <li className={styles.link}>En Beğenilenler</li>
-            </ul>
-          </Grid>
-          <Grid item xs={12} sm={4} md={3}>
-            <h4>İletişim</h4>
-          </Grid>
-        </Grid>
-      </div>
-      <div className={styles.bottom}>
+    <footer
+      className={styles.footer}
+    >
+      <div
+        className={styles.footer__top}
+      >
+        <div
+          className={styles.footer__logo}
+        >
+          <Image 
+          src="/logo.jpg" 
+          alt="logo" 
+          width={150} 
+          height={38} 
+          style={{objectFit: "contain"}}
+          />
+          <p className={styles.logo}>
+            MovieReview 2023 <br />
+            All rights reserved &copy;
+          </p>
+        </div>
+        <div className={styles.footer__links}>
+          <div className={styles.footer__link}>
+              <h3 style={{fontWeight:600}}>
+                Hızlı Linkler
+              </h3>
+              <Link href="/"
+              style={{color:'#6b7280'}}>
+                Diziler
+              </Link>
+              <Link href="/"
+              style={{color:'#6b7280'}}>
+                Filmler
+              </Link>
+              <Link href="/"
+              style={{color:'#6b7280'}}>
+                Oyuncular
+              </Link>
+          </div>
 
-      </div>
-    </div>
-  )
-}
+          <div className={styles.footer__link}>
+              <h3 style={{fontWeight:600}}>
+                Destek
+              </h3>
+              <Link href="/"
+              style={{color:'#6b7280'}}>
+                İletişim
+              </Link>
+              <Link href="/"
+              style={{color:'#6b7280'}}>
+                Kariyer
+              </Link>
+              <Link href="/"
+              style={{color:'#6b7280'}}>
+                Misyonumuz
+              </Link>
+          </div>
+
+          <div className={styles.footer__link}>
+              <h3 style={{fontWeight:600}}>
+                Politikalarımız
+              </h3>
+              <Link href="/"
+              style={{color:'#6b7280'}}>
+                Gizlilik
+              </Link>
+              <Link href="/"
+              style={{color:'#6b7280'}}>
+                Kullanım Şartları
+              </Link>
+              <Link href="/"
+              style={{color:'#6b7280'}}>
+                Çerez Politikası
+              </Link>
+          </div>
+        </div>
+        </div>
+        <div className={styles.footer__bottom}>
+            <p>@2023 MovieReview. All Rights Reserved</p>
+            <div className={styles.footer__bottom__links}>
+                <Link href="/" 
+                className="text-gray-500">
+                    Privacy Policy
+                </Link>
+                <Link href="/" 
+                className="text-gray-500">
+                    Terms of Use
+                </Link>
+            </div>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer;
