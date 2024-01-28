8c38187e0b16bbf2d8358bea23ea70e6cd97a1d6
ferhatergun <eferhat718@gmail.com>
2024-01-28 20:35:50 +0300

    admin logout işlemi yapıldı

--- src/components (Admin)/Navbar/Nabvar.js
+++ src/components (Admin)/Navbar/Nabvar.js
@@ -1,11 +1,21 @@
+"use client"
 import React from 'react'
 import styles from './page.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 import LogoutIcon from '@mui/icons-material/Logout';
 import { Tooltip } from '@mui/material';
+import { toast } from 'react-toastify';
+import { deleteCookie } from 'cookies-next'
 
 export default function Nabvar() {
+
+  const logoutAdmin=()=>{
+    deleteCookie("accessToken")
+    deleteCookie("adminId")
+    window.location.href="/"
+    toast.success("Çıkış Yapıldı")
+  }
   return (
     <div className={styles.container}>
         <div className={styles.rightItems}>
@@ -18,7 +28,7 @@ export default function Nabvar() {
             <Link href="/admin/users">Kullanıcılar</Link> 
         </div>
         <Tooltip title="Çıkış Yap" followCursor>
-            <div className={styles.logoutBtn}>
+            <div className={styles.logoutBtn} onClick={logoutAdmin}>
                 <LogoutIcon/>
             </div>
         </Tooltip>
